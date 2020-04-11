9382ee80b325848a7d23765df1d5903a07ca3ffa
Michael Dimchuk <mdimchuk@atb.com>
2020-04-11 14:21:16 -0700

    Simplified test

--- src/test/java/org/michaeldimchuk/structures/BinarySearchTreeTest.java
+++ src/test/java/org/michaeldimchuk/structures/BinarySearchTreeTest.java
@@ -2,7 +2,8 @@ package org.michaeldimchuk.structures;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.google.common.collect.ImmutableList;
+import java.util.stream.IntStream;
+
 import org.junit.jupiter.api.Test;
 
 class BinarySearchTreeTest {
@@ -11,18 +12,10 @@ class BinarySearchTreeTest {
   void valuesAreAddedCorrectlyTest() {
     BinarySearchTree<Integer> tree = new BinarySearchTree<>();
 
-    for (Integer value : ImmutableList.of(3, 1, 500, 3)) {
+    IntStream.of(3, 1, 500, 3).forEach(value -> {
       tree.add(value);
       assertThat(tree.contains(value)).isTrue();
-      System.out.println("\n\n\n");
-    }
-
-    Integer one = 1;
-
-    Integer two = 1;
-
-    System.out.println("====" + one.compareTo(two));
-
+    });
     assertThat(tree.contains(4)).isFalse();
   }
 }
